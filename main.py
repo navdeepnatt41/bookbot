839168fafbb9b4dd9a23b381954b08f6bd2caebd
Navdeep Natt <nattnavdeep41@gmail.com>
2025-10-26 20:01:52 -0500

    B. Implemented the "count_book_words" function

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def get_book_text(filepath: str) -> str:
+    """This function returns the text in a file as a string."""
+
     with open(filepath, 'r') as f:
         return f.read()
 
+def count_book_words(file_text: str) -> int:
+    """This functions returns the number of words in a string representing the text of a book"""
+
+    return len(file_text.split())
+
 if __name__ == "__main__":
-    print(get_book_text("books/frankenstein.txt"))
+    book_text: str = get_book_text("books/frankenstein.txt")
+    book_word_count: int = count_book_words(book_text)
+    print(f"Found {book_word_count} total words")
