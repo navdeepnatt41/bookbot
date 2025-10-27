a30d5369a96f9475ef166f81db97f4261025ff1d
Navdeep Natt <nattnavdeep41@gmail.com>
2025-10-26 20:04:11 -0500

    C. Refactored code to move "count_book_words" into separate "stats" file

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+from stats import count_book_words
+
 def get_book_text(filepath: str) -> str:
     """This function returns the text in a file as a string."""
 
     with open(filepath, 'r') as f:
         return f.read()
-
-def count_book_words(file_text: str) -> int:
-    """This functions returns the number of words in a string representing the text of a book"""
-
-    return len(file_text.split())
 
 if __name__ == "__main__":
     book_text: str = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_book_words(file_text: str) -> int:
+    """This functions returns the number of words in a string representing the text of a book"""
+
+    return len(file_text.split())
