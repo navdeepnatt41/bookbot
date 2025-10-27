ea555582f45e994cf8cbd7447123fedbbad92e6c
Navdeep Natt <nattnavdeep41@gmail.com>
2025-10-26 20:29:48 -0500

    D. Added a "character_count" function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_book_words
+from stats import count_book_words, character_count
 
 def get_book_text(filepath: str) -> str:
     """This function returns the text in a file as a string."""
@@ -10,3 +10,4 @@ if __name__ == "__main__":
     book_text: str = get_book_text("books/frankenstein.txt")
     book_word_count: int = count_book_words(book_text)
     print(f"Found {book_word_count} total words")
+    print(f"{character_count(book_text)}")
--- stats.py
+++ stats.py
@@ -2,3 +2,18 @@ def count_book_words(file_text: str) -> int:
     """This functions returns the number of words in a string representing the text of a book"""
 
     return len(file_text.split())
+
+def character_count(file_text: str) -> dict[str, int]:
+    """
+    This function returns a dictionary mapping characters to the number
+    of times it they were found in a text.
+    """
+    
+    char_counts: dict[str, int] = {}
+    for c in file_text.lower():
+        if c in char_counts:
+            char_counts[c] += 1
+        else:
+            char_counts[c] = 1
+
+    return char_counts
