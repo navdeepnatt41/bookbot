75967e4b34d43e7df49ace93456c723f9565ef45
Navdeep Natt <nattnavdeep41@gmail.com>
2025-10-26 19:54:34 -0500

    A. Wrote the first function: get_book_txt

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("greetings boots")
+def get_book_text(filepath: str) -> str:
+    with open(filepath, 'r') as f:
+        return f.read()
+
+if __name__ == "__main__":
+    print(get_book_text("books/frankenstein.txt"))
